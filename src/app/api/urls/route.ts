b41f8b16b79d7b4386593b8b8da47b7de28c0ee1
Alena Batitskaia <batickaya.a@gmail.com>
2024-10-08 16:30:16 +0100

    Cache deactivated

--- src/app/api/urls/route.ts
+++ src/app/api/urls/route.ts
@@ -1,20 +1,20 @@
 import { UrlShortenerService } from "@/services/UrlShortenerService";
 import { NextResponse } from "next/server";
-import { cache } from "react";
+// import { cache } from "react";
 
-const fetchUrls = cache(async () => {
+const fetchUrls = async () => {
   const shortenerService = new UrlShortenerService();
   const urls = await shortenerService.getAllUrls();
   return urls;
-});
+};
 
 export async function GET() {
   const urls = await fetchUrls();
   const response = NextResponse.json({ urls });
-  response.headers.set(
-    "Cache-control",
-    "public, max-age=200, s-maxage=200, stale-while-revalidate=59"
-  );
+  // response.headers.set(
+  //   "Cache-control",
+  //   "public, max-age=200, s-maxage=200, stale-while-revalidate=59"
+  // );
 
   return response;
 }
