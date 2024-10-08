0534485759de74f66239b169b1b72765b04e88a9
Alena Batitskaia <batickaya.a@gmail.com>
2024-10-08 14:51:32 +0100

    Meta changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "✂️ URL Shortener",
-  description: "Alёna's personal URL shortener service",
+  title: "URL Customizer",
+  description: "Alёna's personal service for making custom URLs",
 };
 
 export default function RootLayout({
