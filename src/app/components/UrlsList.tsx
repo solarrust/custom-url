fbe6980e129d3fecd5eef92b75b6f6aea200873d
Alena Batitskaia <batickaya.a@gmail.com>
2024-10-09 16:00:31 +0100

    Flip flop urls list (reversed)

--- src/app/components/UrlsList.tsx
+++ src/app/components/UrlsList.tsx
@@ -33,32 +33,34 @@ export default async function UrlsList() {
         </thead>
         <tbody>
           {urls.urls &&
-            urls.urls.map(
-              (url: {
-                _id: string;
-                originalUrl: string;
-                shortUrl: string;
-                visits: number;
-              }) => (
-                <tr key={url._id}>
-                  <td>
-                    <a href={`${url.originalUrl}`} target="_blank">
-                      {url.originalUrl}
-                    </a>
-                  </td>
-                  <td>
-                    <a href={`${url.shortUrl}`} target="_blank">
-                      {`${process.env.BASE_URL}${url.shortUrl}`}
-                    </a>
-                    <CopyButton
-                      url={`${process.env.BASE_URL}${url.shortUrl}`}
-                    />
-                    <DeleteButton id={url._id} />
-                  </td>
-                  <td>{url.visits}</td>
-                </tr>
-              )
-            )}
+            urls.urls
+              .toReversed()
+              .map(
+                (url: {
+                  _id: string;
+                  originalUrl: string;
+                  shortUrl: string;
+                  visits: number;
+                }) => (
+                  <tr key={url._id}>
+                    <td>
+                      <a href={`${url.originalUrl}`} target="_blank">
+                        {url.originalUrl}
+                      </a>
+                    </td>
+                    <td>
+                      <a href={`${url.shortUrl}`} target="_blank">
+                        {`${process.env.BASE_URL}${url.shortUrl}`}
+                      </a>
+                      <CopyButton
+                        url={`${process.env.BASE_URL}${url.shortUrl}`}
+                      />
+                      <DeleteButton id={url._id} />
+                    </td>
+                    <td>{url.visits}</td>
+                  </tr>
+                )
+              )}
         </tbody>
       </table>
     </div>
